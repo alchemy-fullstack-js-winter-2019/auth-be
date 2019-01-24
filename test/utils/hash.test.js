45026696f21031f2e021126c767d72be35d9271d
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-24 10:41:59 -0800

    second test passing

--- test/utils/hash.test.js
+++ test/utils/hash.test.js
@@ -2,7 +2,19 @@ const bcrypt = require('bcryptjs');
 
 describe('hash tests', () => {
   it('hashes a password', () => {
-    const password = bcrypt.hash('password', 10);
+    bcrypt.hash('password', 10)
+    .then(hashedPassword => {
+      expect(hashedPassword).toBeDefined();
+    })
+  });
+  it('creates hashed passwords that are different', () => {
+    bcrypt.hash('password', 10)
+    .then(hashedPass1 => {
+      bcrypt.hash('password', 10)
+      .then(hashedPass2 => {
+        expect(hashedPass1).not.toEqual(hashedPass2);
+      })
+    })
   })
 })
 
