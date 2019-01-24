84f946bb5067c282b860c7b2c72404d7e5e87a58
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-24 15:17:13 -0800

    added dependencies

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,7 +1,15 @@
+require('dotenv').config();
+require('../../lib/utils/connect')();
+const mongoose = require('mongoose');
 const { Types } = require('mongoose');
 const User = require('../../lib/models/User');
 
 describe('User model', () => {
+
+  beforeEach(() => {
+    mongoose.connection.dropDatabase(done);
+  });
+
   it('validates a good model', () => {
     const user = new user({ email: 'test@test.com' });
     expect(user.JSON).toEqual({ email: 'test@test.com', _id: expect.any(Object) });
@@ -11,5 +19,27 @@ describe('User model', () => {
     const user = new User({});
     const errors = user.validateSync();
     expect(errors.email.message).toEqual('');
+  });
+
+  it('stores an _tempPassword', () => {
+    const user = new User({ email: 'test@test.com', password: 'PassWord' });
+    expect(user._tempPassword).toEqual('PassWord');
+  });
+
+  it('has a passwordHash', () => {
+    // const user = new User({
+    //   email: 'test@test.com',
+    //   password: 'PassWord'
+    // });
+    // user.save();
+
+    return User.create({
+      email: 'test@test.com',
+      password: 'PassWord'
+    })
+      .then(user => {
+        expect(user.passwordHash).toEqual(expect.any(String));
+        expect(user.password).toBeUndefined();
+      });
   });
 });
