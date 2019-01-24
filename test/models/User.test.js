0b26a849010c225dfa7d14fc67e8e88bb0af758f
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-24 14:28:56 -0800

    temp password works

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -11,4 +11,8 @@ describe('user model tests', () => {
     const errors = user.validateSync().errors;
     expect(errors.email.message).toEqual('Email required');
   });
+  it('stores a _tempPassword', () => {
+    const user = new User({ email: 'test@test.com', password: 'abc' });
+    expect(user._tempPassword).toEqual('abc');
+  });
 });
