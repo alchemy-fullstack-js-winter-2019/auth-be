9b63a68e650adc2a23d7f14ce121695123bab02b
Kristin Hortsch <kristinhortsch@gmail.com>
2019-01-24 10:42:47 -0800

    2nd brcypt classwork test passing

--- test/utils/hash.test.js
+++ test/utils/hash.test.js
@@ -2,10 +2,25 @@ const bcryptjs = require('bcryptjs');
 
 describe('bcrypt', () => {
   it('hashes a password', () => {
-    bcryptjs.hash('password', 10)
+    return bcryptjs.hash('password', 10)
       .then(hashedPassword => {
         expect(hashedPassword).toBeDefined();
         console.log(hashedPassword);      
       });
   });
+
+  it('creates hashed passwords that are different', () => {
+    const password = 'roxy';
+    return bcryptjs.hash(password, 10)
+      .then(hashedPassword1 => {
+        return bcryptjs.hash(password, 10)
+          .then(hashedPassword2 => {
+            expect(hashedPassword1).not.toEqual(hashedPassword2);
+          });
+      });
+  });
+
+  // it('creates the same hash given the same salt', () => {
+  //   const salt = $2b$10$fkdlskei3lsjvbcneosntl
+  // })
 });
