a3524ef8959e7927ecc44e17116ae693fc255ac8
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-24 14:20:56 -0800

    making email required

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -6,4 +6,10 @@ describe('User model', () => {
     const user = new user({ email: 'test@test.com' });
     expect(user.JSON).toEqual({ email: 'test@test.com', _id: expect.any(Object) });
   });
+
+  it('has a required email', () => {
+    const user = new User({});
+    const errors = user.validateSync();
+    expect(errors.email.message).toEqual('');
+  });
 });
