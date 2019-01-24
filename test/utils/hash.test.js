2a603ab2a65afaeb794a1e1acb74b2a131a98be1
Paige Gorry <paigeegorry@gmail.com>
2019-01-24 10:48:45 -0800

    changed chaining thens to a Promise.all([Promise.resolve()])

--- test/utils/hash.test.js
+++ test/utils/hash.test.js
@@ -4,27 +4,32 @@ describe('bcrypt', () => {
   it('hashes a password', () => {
     return bcrypt.hash('password', 10)
       .then(hashedPassword => {
-        console.log(hashedPassword);
         expect(hashedPassword).toBeDefined();
       });
   });
   it('creates hashed passwords that are different', () => {
     return bcrypt.hash('password', 10)
       .then(password1 => {
-        return bcrypt.hash('password', 10)
-          .then(password2 => {
-            expect(password1).not.toEqual(password2);
-          });
+        return Promise.all([
+          Promise.resolve(password1),
+          bcrypt.hash('password', 10)
+        ]);
+      })
+      .then(([hash1, hash2]) => {
+        expect(hash1).not.toEqual(hash2);
       });
   });
   it('creates the same hash given the same salt', () => {
     const salt = '$2b$10$12jne9skdn3jdkskwopa98';
     return bcrypt.hash('password', salt)
       .then(password1 => {
-        return bcrypt.hash('password', salt)
-          .then(password2 => {
-            expect(password1).toEqual(password2);
-          });
+        return Promise.all([
+          Promise.resolve(password1),
+          bcrypt.hash('password', salt)
+        ]);
+      })
+      .then(([hash1, hash2]) => {
+        expect(hash1).toEqual(hash2);
       });
   });
 });
