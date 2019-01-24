de9ea29bb1caad05806b9f0244458ef863ef76c1
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-24 13:59:49 -0800

    user model and preliminary tests

---
+++ tests/models/User.test.js
@@ -0,0 +1,17 @@
+const bcrypyt = require('bcryptjs');
+const User = require('../../lib/models/User');
+
+describe('user model', () => {
+  it('validates a good model', () => {
+    const user = new User({ email: 'test@test.com' });
+    expect(user.toJSON()).toEqual({ email: 'test@test.com', _id: expect.any(Object) });
+    
+  });
+
+  it('has a required email', () => {
+    const user = new User({});
+    const errors = user.validateSync().errors
+  });
+});
+
+
