d723ad38c77efb6ebbfd04341c084da3fad2a232
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-24 15:28:24 -0800

    all tests passing

--- tests/models/User.test.js
+++ tests/models/User.test.js
@@ -1,10 +1,19 @@
-const bcrypyt = require('bcryptjs');
+require('dotenv').config();
+require('../../lib/utils/connect')();
+const mongoose = require('mongoose');
+const { Types } = require('mongoose');
+
 const User = require('../../lib/models/User');
 
 describe('user model', () => {
+
+  beforeEach(done => {
+    mongoose.connection.dropDatabase(done);
+  });
+
   it('validates a good model', () => {
     const user = new User({ email: 'test@test.com' });
-    expect(user.toJSON()).toEqual({ email: 'test@test.com', _id: expect.any(Object) });
+    expect(user.toJSON()).toEqual({ email: 'test@test.com', _id: expect.any(Types.ObjectId) });
     
   });
 
@@ -12,6 +21,7 @@ describe('user model', () => {
     const user = new User({});
     const errors = user.validateSync().errors;
     expect(errors.email.message).toEqual('Path `email` is required.');
+    // eslint-disable-next-line no-console
     console.log(errors);
   });
 
@@ -20,9 +30,29 @@ describe('user model', () => {
     expect(user._tempPassword).toEqual('password');
   });
 
-  it('finds a user by token', () => {
+  it('password hash is stored after user.save', () => {
+    const user = new User({ email: 'test@test.com', password: 'password' });
+    user.save();
+    expect(user).toContain('password');
+  });
+
+  it('has a passwordHash', () => {
+  
+    return User.create({ 
+      email: 'test@test.com', 
+      password: 'password' 
+    })
+      .then(user => {
+        expect(user.passwordHash).toEqual(expect.any(String));
+        expect(user.password).toBeUndefined();
+      });
+    
+  });
+
+  it('can create a static findByToken method', () => {
 
   });
+
 });
 
 
