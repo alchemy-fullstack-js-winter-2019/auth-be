f229f6b43c139c40985c0bfb7689db7f56a0aa5b
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-24 11:49:16 -0800

    verify test passes

--- test/utils/token.test.js
+++ test/utils/token.test.js
@@ -5,5 +5,10 @@ describe('jwt functions', () => {
     const token = jwt.sign({ payload: { hi: 'there' } }, 'secret');
     expect(token).toEqual(expect.any(String));
   });
+  it('can create and then verify token', () => {
+    const token = jwt.sign({ payload: { hi: 'there' } }, 'secret');
+    const body = jwt.verify(token, 'secret');
+    expect(body).toEqual({ payload: { hi: 'there' }, iat: expect.any(Number) });
+  });
 });
 
