de49bd72a78b99194893db068fd3b1be85a32805
Paige Gorry <paigeegorry@gmail.com>
2019-01-24 10:40:19 -0800

    passwords with the same salt are equal to each other

--- test/utils/hash.test.js
+++ test/utils/hash.test.js
@@ -2,18 +2,28 @@ const bcrypt = require('bcryptjs');
 
 describe('bcrypt', () => {
   it('hashes a password', () => {
-    bcrypt.hash('password', 10)
-      .then(password => {
-        console.log(password);
-        expect(password).toBeDefined();
+    return bcrypt.hash('password', 10)
+      .then(hashedPassword => {
+        console.log(hashedPassword);
+        expect(hashedPassword).toBeDefined();
       });
   });
   it('creates hashed passwords that are different', () => {
-    bcrypt.hash('password', 10)
+    return bcrypt.hash('password', 10)
       .then(password1 => {
-        bcrypt.hash('password', 10)
+        return bcrypt.hash('password', 10)
           .then(password2 => {
             expect(password1).not.toEqual(password2);
+          });
+      });
+  });
+  it('creates the same hash given the same salt', () => {
+    const salt = '$2b$10$12jne9skdn3jdkskwopa98';
+    return bcrypt.hash('password', salt)
+      .then(password1 => {
+        return bcrypt.hash('password', salt)
+          .then(password2 => {
+            expect(password1).toEqual(password2);
           });
       });
   });
