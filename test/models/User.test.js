b2eb879caafbcc28f1b2e7b001152f7567876f5d
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-24 14:09:57 -0800

    requried email test passing

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,9 +1,14 @@
-const mongoose = require('mongoose');
+const { Types } = require('mongoose');
 const User = require('../../lib/models/User');
 
 describe('user model tests', () => {
   it('validates a good model', () => {
     const user = new User({ email: 'test@test.com' });
-    expect(user.toJSON()).toEqual({ email: 'test@test.com', _id: expect.any(mongoose.Types.ObjectId) });
+    expect(user.toJSON()).toEqual({ email: 'test@test.com', _id: expect.any(Types.ObjectId) });
+  });
+  it('has a required email', () => {
+    const user = new User({});
+    const errors = user.validateSync().errors;
+    expect(errors.email.message).toEqual('Email required');
   });
 });
