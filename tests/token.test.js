99c826fe52c7b8e00858e7b3fc16a278706820e9
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-24 11:54:46 -0800

    verify a token with an expiration

--- tests/token.test.js
+++ tests/token.test.js
@@ -11,4 +11,10 @@ describe('jwt functions', () => {
     const body = jwt.verify(token, 'secret');
     expect(body).toEqual({ payload: { hi: 'there' }, iat: expect.any(Number) });
   });
+
+  it('can verify a token with expiration', () => {
+    const token = jwt.sign({ payload: { hi: 'there' } }, 'secret');
+    const body = jwt.verify(token, 'secret', { expiresIn: '1h' });
+    expect(body).toEqual({ payload: { hi: 'there' }, iat: expect.any(Number), exp: expect.any(Number) });
+  });
 });
