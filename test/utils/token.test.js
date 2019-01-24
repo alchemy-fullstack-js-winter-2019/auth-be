ea00698a5e44208ef9b916e7165b2374f98e1e5e
Kristin Hortsch <kristinhortsch@gmail.com>
2019-01-24 11:57:56 -0800

    passing rest of token tests

--- test/utils/token.test.js
+++ test/utils/token.test.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { tokenize, untokenize } = require('../../lib/utils/token');
 
 describe('json web tokens', () => {
   it('can create a token', () => {
@@ -9,13 +11,30 @@ describe('json web tokens', () => {
   it('can verify a token', () => {
     const token = jwt.sign({ payload: { hi: 'cats' } }, 'secret');
     const body = jwt.verify(token, 'secret');
-    expect(body).toEqual({ payload: { hi: 'cats' }, iat: expect.any(Number) });
+    expect(body).toEqual({ 
+      payload: { hi: 'cats' }, 
+      iat: expect.any(Number) });
   });
 
   it('can verify a token with expiration', () => { 
     const token = jwt.sign({ payload: { hi: 'fulldarknostars' } }, 'secret', { expiresIn: '1h' });
     const body = jwt.verify(token, 'secret', { expiresIn: '1h' });
-    expect(body).toEqual({ payload: { hi: 'fulldarknostars' }, iat: expect.any(Number), exp: expect.any(Number) });
-    
+    expect(body).toEqual({ 
+      payload: { hi: 'fulldarknostars' }, 
+      iat: expect.any(Number), 
+      exp: expect.any(Number) });
+  });
+
+  it('can use a tokenize function', () => {
+    const payload = { payload: { hi: 'there' } };
+    const token = tokenize(payload);
+    expect(token).toEqual(expect.any(String));
+  });
+  
+  it('can use untokenize to return a payload from a given token', () => {
+    const payload = { payload: { hi: 'there' } };
+    const token = tokenize(payload);
+    const tokenNoMore = untokenize(token);
+    expect(tokenNoMore).toEqual({ hi: 'there' });
   });
 });
