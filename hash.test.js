1a2b5e361977549be60838c312524405efc1b292
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-24 10:43:14 -0800

    creates hashed passwords that are different

--- hash.test.js
+++ hash.test.js
@@ -4,7 +4,18 @@ describe('hashing functions', () => {
   it('hashes a password', () => {
     return bcrypyt.hash('password', 10)
       .then(hashedPassword => {
-        expect(hashedPassword).toEqual('');
+        expect(hashedPassword).toBeDefined;
+      });
+  });
+
+  it('creates hashed passwords that are different', () => {
+    const password = 'password';
+    bcrypyt.hash(password, 10)
+      .then(hashedPassword1 => {
+        return bcrypyt.hash(password, 10)
+          .then(hashedPassword2 => {
+            expect(hashedPassword1).toEqual(hashedPassword2);
+          });
       });
   });
 });
