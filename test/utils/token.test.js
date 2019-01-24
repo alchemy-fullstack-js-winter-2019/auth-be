b6e2be44090161985a4c0fe39908287fce4bfa4d
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-24 11:43:02 -0800

    create token function passing

---
+++ test/utils/token.test.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken');
+
+describe('jwt functions', () => {
+  it('can create a token', () => {
+    const token = jwt.sign({ payload: { hi: 'there' } }, 'secret');
+    expect(token).toEqual(expect.any(String));
+  });
+});
+
