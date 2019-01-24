c41c04021ef9cbabb3f7f2ab2f657b23b77671f7
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-24 14:11:18 -0800

    added email validation to user schema

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -16,4 +16,12 @@ describe('User', () => {
     expect(errors).toBeDefined();
     expect(errors.email['message']).toEqual('Path `email` is required.');
   });
+
+  it('can require an email to match a pattern', () => {
+    const user = new User({ email: 'test' });
+    const errors = user.validateSync().errors;
+    expect(errors.email['message']).toEqual(
+      '\'test\' is not a valid email!'
+    );
+  });
 });
