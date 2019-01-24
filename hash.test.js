b800e4368f378e55aff2ffae0a8764dba25bcb1b
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-24 10:35:21 -0800

    hash test

--- hash.test.js
+++ hash.test.js
@@ -3,5 +3,8 @@ const bcrypyt = require('bcryptjs');
 describe('hashing functions', () => {
   it('hashes a password', () => {
     return bcrypyt.hash('password', 10)
+      .then(hashedPassword => {
+        expect(hashedPassword).toEqual('');
+      });
   });
 });
