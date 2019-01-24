a0c11c62caa15584d040194d3addbe489e71aebe
Shaba9 <shaba92@gmail.com>
2019-01-24 12:01:50 -0800

    creates different hashes passes

--- test/utils/hash.test.js
+++ test/utils/hash.test.js
@@ -5,4 +5,15 @@ describe('hashing', () => {
     bcrypt.hash('password', 10)
       .then(hashedPassword => expect(hashedPassword).toBeDefined());
   });
+
+  it('creates hashed passwords that are different', () => {
+    bcrypt.hash('password', 10)
+      .then(hashedPassword1 => {
+        return Promise.all([
+          Promise.resolve(hashedPassword1),
+          bcrypt.hash('password', 10)
+        ]);
+      })
+      .then(([hashedPassword1, hashedPassword2]) => expect(hashedPassword1).to.not.Equal(hashedPassword2));
+  });
 });
