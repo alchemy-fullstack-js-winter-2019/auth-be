51aea903b45c3ce7d325ce5ed37c22b42e20a50f
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-24 11:46:57 -0800

    create a token

---
+++ tests/token.test.js
@@ -0,0 +1,8 @@
+const jwt = require('jsonwebtoken');
+
+describe('jwt functions', () => {
+  it('can create a token', () => {
+    const token = jwt.sign({ payload: { hi: 'there' } }, 'secret');
+    expect(token).toEqual('');
+  });
+});
