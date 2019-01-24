3f12f1bc0b41754a277143ea13845d5d46db771d
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-24 10:55:33 -0800

    encrypt words with a given salt

--- hash.test.js
+++ hash.test.js
@@ -18,4 +18,19 @@ describe('hashing functions', () => {
           });
       });
   });
+
+  it('creates the same hash given the same salt', () => {
+    const password = 'password';
+    const salt = $2b$10$2222222222222222222222;
+    return bcrypyt.hash('password', salt)
+      .then(hashedPassword => {
+        return Promise.all([
+          Promise.resolve(hashedPassword),
+          bcrypt.hash(password, salt)
+        ]);
+      })
+      .then(([hash1, hash2]) => {
+        expect(hash1).toEqual(hash2);
+      });
+  });
 });
