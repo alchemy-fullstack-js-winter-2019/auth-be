68d4d8a42d9913e97a704a96dcd02d440822b4e0
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-24 15:11:51 -0800

    user.save test is working

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,7 +1,14 @@
+require('dotenv').config();
+require('../../lib/utils/connect')();
 const { Types } = require('mongoose');
 const User = require('../../lib/models/User');
+const mongoose = require('mongoose');
 
 describe('user model tests', () => {
+  beforeEach(done => {
+    mongoose.connection.dropDatabase(done);
+  });
+
   it('validates a good model', () => {
     const user = new User({ email: 'test@test.com' });
     expect(user.toJSON()).toEqual({ email: 'test@test.com', _id: expect.any(Types.ObjectId) });
@@ -14,5 +21,13 @@ describe('user model tests', () => {
   it('stores a _tempPassword', () => {
     const user = new User({ email: 'test@test.com', password: 'abc' });
     expect(user._tempPassword).toEqual('abc');
+  });
+  it('password hash is set after user.save', () => {
+    const user = new User({ email: 'test@test.com', password: 'abc' });
+    user.save()
+      .then(user => {
+        expect(user.passwordHash).toEqual(expect.any(String));
+        expect(user.password).toBeUndefined();
+      });
   });
 });
--- test/utils/hash.test.js
+++ test/utils/hash.test.js
@@ -4,46 +4,46 @@ const { hash, compare } = require('../../lib/utils/hash');
 describe('hash tests', () => {
   it('hashes a password', () => {
     return bcrypt.hash('password', 10)
-    .then(hashedPassword => {
-      expect(hashedPassword).toBeDefined();
-    })
+      .then(hashedPassword => {
+        expect(hashedPassword).toBeDefined();
+      });
   });
   it('creates hashed passwords that are different', () => {
     return bcrypt.hash('password', 10)
-    .then(hashedPass1 => {
-      return bcrypt.hash('password', 10)
-      .then(hashedPass2 => {
-        expect(hashedPass1).not.toEqual(hashedPass2);
-      })
-    })
+      .then(hashedPass1 => {
+        return bcrypt.hash('password', 10)
+          .then(hashedPass2 => {
+            expect(hashedPass1).not.toEqual(hashedPass2);
+          });
+      });
   });
-  it('it creates the same hash given the same salt',() => {
+  it('it creates the same hash given the same salt', () => {
     const salt = '$2b$10$1234567891234567891234';
     return bcrypt.hash('password', salt)
       .then(hashed1 => {
         return bcrypt.hash('password', salt)
           .then(hashed2 => {
-            expect(hashed1).toEqual(hashed2)
-          })
-      })
+            expect(hashed1).toEqual(hashed2);
+          });
+      });
   });
   it('it can compare hashes based on the same password', () => {
     return bcrypt.hash('password', 10)
       .then(hashed => {
         return bcrypt.compare('password', hashed)
-        .then(result => {
-          expect(result).toBeTruthy();
-        })
-      })
+          .then(result => {
+            expect(result).toBeTruthy();
+          });
+      });
   });
   it('it can compare hashes based on different password', () => {
     return bcrypt.hash('password', 10)
       .then(hashed => {
         return bcrypt.compare('WRONG PASSWORD', hashed)
-        .then(result => {
-          expect(result).toBeFalsy();
-        })
-      })
+          .then(result => {
+            expect(result).toBeFalsy();
+          });
+      });
   });
   it('uses hash function that returns a hashed password', () => {
     return hash('password')
@@ -57,8 +57,8 @@ describe('hash tests', () => {
         return compare('password', hashedPassword)
           .then(res => {
             expect(res).toBeTruthy();
-          })
-      })
+          });
+      });
   });
   it('uses compare function to check if two different hashes are NOT the same', () => {
     return hash('password')
@@ -66,8 +66,8 @@ describe('hash tests', () => {
         return compare('WRONGpassword', hashedPassword)
           .then(res => {
             expect(res).toBeFalsy();
-          })
-      })
+          });
+      });
   });
-})
+});
 
