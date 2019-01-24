fdbae1eb63ac359f53ad53b345c05c8593b8b19a
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-24 11:50:36 -0800

    verify a token test

--- tests/token.test.js
+++ tests/token.test.js
@@ -5,4 +5,10 @@ describe('jwt functions', () => {
     const token = jwt.sign({ payload: { hi: 'there' } }, 'secret');
     expect(token).toEqual('');
   });
+
+  it('can verify a token', () => {
+    const token = jwt.sign({ payload: { hi: 'there' } }, 'secret');
+    const body = jwt.verify(token, 'secret');
+    expect(body).toEqual({ payload: { hi: 'there' }, iat: expect.any(Number) });
+  });
 });
