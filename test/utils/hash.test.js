b435883421b81ec7bf832dd3d44a99694e9b7916
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-24 10:47:05 -0800

    encrypt another password test passes

--- test/utils/hash.test.js
+++ test/utils/hash.test.js
@@ -8,4 +8,20 @@ describe('auth-be app', () => {
       expect(hashedPassword).toBeDefined()
     })
   });
+
+  it('creates hashed passwords that are different', () => {
+    const password = 'password';
+    return bcryptjs.hash(password, 10)
+    .then(hashedPassword1 => {
+      return Promise.all([
+        Promise.resolve(hashedPassword1),
+        bcryptjs.hash(password, 10)
+      ]);
+    })
+    .then(([hash1, hash2]) => {
+      console.log(hash1, hash2)
+      expect(hash1).not.toEqual(hash2);
+    });
+  });
+
 });
