f26bfba106df91cc0356deee9941c674305bf952
Lance Owen Merrill <Lanceom@hawaii.edu>
2019-01-24 15:28:55 -0800

    hashed password gets saved from clear password

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,9 +1,17 @@
-// const mongoose = require('mongoose');
+require('dotenv').config();
+require('../../lib/utils/connect')();
+const mongoose = require('mongoose');
 const User = require('../.././lib/models/User');
 
 
 
 describe('test the user model', () => {
+    beforeEach(done => {
+        return mongoose.connection.dropDatabase(() => {
+            done();
+        });
+    });
+
     it('validates a good model', () => {
         const user  = new User({ email: 'LOL@gmail.com' });
         expect(user.toJSON()).toEqual({ _id: expect.any(Object), email: 'LOL@gmail.com' });
@@ -14,5 +22,16 @@ describe('test the user model', () => {
         console.log(errors.email.message);
         expect(errors.email.message).toEqual('Path `email` is required.');
 
+    });
+    it('stores a temp password', () => {
+        const user = new User({ email: 'LOL@gmail.com', password: 'YES' });
+        expect(user._tempPassword).toEqual('YES');
+    });
+    it('saves the hashed clear password', () => {
+        const user = new User({ email: 'LOL@gmail.com', password: 'YES' });
+        user.save()
+            .then(user => {
+                expect(user.passwordHash).toEqual(expect.any(String));
+            });
     });
 });
