2cfa4647387bf5e6ba4f3ac59ca66ec92201ff8c
Paige Gorry <paigeegorry@gmail.com>
2019-01-24 10:33:21 -0800

    expect hashed password1 not to equal hashed password2

--- test/utils/hash.test.js
+++ test/utils/hash.test.js
@@ -8,4 +8,13 @@ describe('bcrypt', () => {
         expect(password).toBeDefined();
       });
   });
+  it('creates hashed passwords that are different', () => {
+    bcrypt.hash('password', 10)
+      .then(password1 => {
+        bcrypt.hash('password', 10)
+          .then(password2 => {
+            expect(password1).not.toEqual(password2);
+          });
+      });
+  });
 });
