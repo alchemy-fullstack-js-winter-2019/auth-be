651f990fcf52e4cfb55094d9730782ebb6c7c206
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2019-01-24 15:17:16 -0800

    saved hashed password working

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,7 +1,13 @@
+require('dotenv').config();
+require('../../lib/utils/connect')();
 const mongoose = require('mongoose');
+
 const User = require('../../lib/models/User');
 
 describe('user model', () => {
+  beforeEach(done => {
+    mongoose.connection.dropDatabase(done);
+  });
   it('validates a good model', () => {
     const user = new User({ email: 'test@test.com' });
     expect(user.toJSON()).toEqual({ email: 'test@test.com', _id: expect.any(mongoose.Types.ObjectId) });
@@ -14,5 +20,13 @@ describe('user model', () => {
   it('stores a _tempPassword', () => {
     const user = new User({ email: 'test@test.com', password: 'PWORD' });
     expect(user._tempPassword).toEqual('PWORD');
+  });
+  it('can save a hashed password', () => {
+    const user = new User({ email: 'test@test.com', password: 'PASSWORD' });
+    user.save()
+      .then(user => {
+        expect(user.passwordHash).toEqual(expect.any(String));
+        expect(user.password).toBeUndefined();
+      });
   });
 });
