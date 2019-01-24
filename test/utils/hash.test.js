77cf17ed60dc6acc2ddba905db4031cf019f599c
Shaba9 <shaba92@gmail.com>
2019-01-24 13:58:41 -0800

    same hash same salt test pass

--- test/utils/hash.test.js
+++ test/utils/hash.test.js
@@ -7,13 +7,29 @@ describe('hashing', () => {
   });
 
   it('creates hashed passwords that are different', () => {
-    bcrypt.hash('password', 10)
+    return bcrypt.hash('password', 10)
       .then(hashedPassword1 => {
         return Promise.all([
           Promise.resolve(hashedPassword1),
           bcrypt.hash('password', 10)
         ]);
       })
-      .then(([hashedPassword1, hashedPassword2]) => expect(hashedPassword1).to.not.Equal(hashedPassword2));
+      .then(([hashedPassword1, hashedPassword2]) => expect(hashedPassword1).not.toEqual(hashedPassword2));
+  });
+
+  it('creates the same hash given the same salt', () => {
+    const password = 'password';
+    const versionInfo = '$2b$10$';
+    const salt = 'ABCDEFGHIJKLMNOPQRSTUV';
+    const bcryptSalt = `${versionInfo}${salt}`;
+
+    return bcrypt.hash(password, bcryptSalt)
+      .then(hashedPassword => {
+        return Promise.all([
+          Promise.resolve(hashedPassword),
+          bcrypt.hash(password, bcryptSalt)
+        ]);
+      })
+      .then(([hash1, hash2]) => expect(hash1).toEqual(hash2));
   });
 });
