75821780e0dad97f38af99ba9669a1f5604c3ff3
Kristin Hortsch <kristinhortsch@gmail.com>
2019-01-24 15:03:07 -0800

    udpated user pieces

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -15,8 +15,8 @@ describe('user model', () => {
   it('has a required email', () => {
     const user = new User({});
     const errors = user.validateSync().errors;
-    console.log(errors);
     expect(errors).toBeTruthy();
+    expect(errors.email.message).toEqual('Path `email` is required.');
   });
 
   it('must be a valid email address entered', () => {
@@ -24,11 +24,40 @@ describe('user model', () => {
       email: 'invalidemail'
     });
     const errors = user.validateSync().errors;
-    console.log(errors);
     expect(errors).toBeTruthy();
+    expect(errors.email.message).toEqual('email is invalid');
   });
 
   it('stores a _tempPassword', () => {
+    const user = new User({
+      email: 'test@gmail.com',
+      password: 'password'
+    });
+    expect(user._tempPassword).toEqual('password');
+  });
 
+  it('has a passwordHash', () => {
+    const user = new User({
+      email: 'test@gmail.com',
+      password: 'password'
+    });
+    user.save()
+      .then(user => {
+        expect(user.passwordHash).toBeDefined();
+        expect(user.password).toBeUndefined();
+      });
+
+  });
+
+  it('returns true when clear text password matches the password hash', () => {
+    const password = 'password';
+    const user = User.create({ email: 'test@test.com', password });
+    expect(user.compare(password)).toBeTruthy();
+  });
+
+  it('returns false when clear text password doesn\'t match the password hash', () => {
+    const password = 'password';
+    const user = User.create({ email: 'test@test.com', password });
+    expect(user.compare('wrongpassword')).toBeFalsy();
   });
 });
