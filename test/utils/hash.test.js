30019384e70a1598910209cf65f05f3861924377
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-24 11:12:10 -0800

    made initial files and tests

---
+++ test/utils/hash.test.js
@@ -0,0 +1,65 @@
+const bcrypt = require('bcryptjs');
+
+describe('hashing functions', () => {
+  it('hashes a password', () => {
+    return bcrypt.hash('password', 10)
+      .then(hashedPassword => {
+        expect(hashedPassword).toBeDefined();
+      });
+  });
+
+  it('creates hashed passwords that are different', () => {
+    const password = 'password';
+    return bcrypt.hash(password, 10)
+      .then(hashedPassword1 => {
+        return Promise.all([
+          Promise.resolve(hashedPassword1),
+          bcrypt.hash(password, 10)
+        ]);
+      })
+      .then(([hash1, hash2]) => {
+        expect(hash1).not.toEqual(hash2);
+      });
+  });
+
+  it('creates the same has given the same salt', () => {
+    const password = 'password';
+    const versionInfo = '$2b@10$';
+    const salt = 'ABCDEFGHIJKLMNOPQRSTUV';
+    const bcryptSalt = `${versionInfo}${salt}`;
+    return bcrypt.hash(password, bcryptSalt)
+      .then(hashedPassword => {
+        return Promise.all([
+          Promise.resolve(hashedPassword),
+          bcrypt.hash(password, bcryptSalt)
+        ]);
+      })
+      .then(([hash1, hash2]) => {
+        expect(hash1).toEqual(hash2);
+      });
+  });
+
+  it('can compare hashes based on the same password', () => {
+    const password = 'password';
+
+    return bcrypt.hash(password, 10)
+      .then(hashedpassword => {
+        return bcrypt.compare(password, hashedpassword);
+      })
+      .then(result => {
+        expect(result).toBeTruthy();
+      });
+  });
+
+  it('can compare hashes based on the different passwords', () => {
+    const password = 'password';
+
+    return bcrypt.hash(password, 10)
+      .then(hashedpassword => {
+        return bcrypt.compare('badPassword', hashedpassword);
+      })
+      .then(result => {
+        expect(result).toBeFalsy();
+      });
+  });
+});
