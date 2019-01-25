4c08e26ffe82afffae9bd40a16c87f1628db1e01
avanvalk <drewvv@gmail.com>
2019-01-24 16:13:59 -0800

    user file in progress

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,20 +1,41 @@
+require('dotenv').config();
+require('../../lib/utils/connect')();
+const mongoose = require('mongoose');
+const { Types } = require('mongoose');
 const User = require('../../lib/models/User');
 
-describe('hashing functions', () => {
+describe('User model', () => {
+  beforeEach(done => {
+    mongoose.connection.dropDatabase(done);
+  });
+
   it('validates a good model', () => {
     const user = new User({ email: 'test@test.com' });
-    expect(user.toJSON()).toEqual({ email: 'test@test.com', _id: expect.any(Object) });
+    expect(user.toJSON()).toEqual({ email: 'test@test.com', _id: expect.any(Types.ObjectId) });
   });
+
   it('has a required email', () => {
     const user = new User({});
     const errors = user.validateSync().errors;
-    expect(errors.email.message).toEqual('Path `email` is required.');
+    expect(errors.email.message).toEqual('Email required');
   });
+
   it('stores a _tempPassword', () => {
-    const user = new User({ 
-      email: 'test@test.com', 
-      password: 'password' 
+    const user = new User({
+      email: 'test@test.com',
+      password: 'password'
     });
     expect(user._tempPassword).toEqual('password');
+  });
+
+  it('has a passwordHash', () => {
+    return User.create({
+      email: 'test@test.com',
+      password: 'password'
+    })
+      .then(user => {
+        expect(user.passwordHash).toEqual(expect.any(String));
+        expect(user.password).toBeUndefined();
+      });
   });
 });
