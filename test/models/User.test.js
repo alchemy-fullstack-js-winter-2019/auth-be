3f9da93c5cc1aa7220388a4a9829984bb3833586
Carmen Perezchica <crperezchica@gmail.com>
2019-01-24 17:56:12 -0800

    user, token and hash test passing

---
+++ test/models/User.test.js
@@ -0,0 +1,110 @@
+require('dotenv').config();
+require('../../lib/utils/connect')();
+const mongoose = require('mongoose');
+const { Types } = require('mongoose');
+const User = require('../../lib/models/User'); //importing our user
+const { tokenize, untokenize } = require('../../lib/utils/token');
+
+
+
+describe('models', () => {
+  beforeEach((done) => { 
+    mongoose.connection.dropDatabase(done);
+  });
+  it('validates a good model', () => {
+    const user = new User({ email: 'test@test.com' }); //creating a new user and pass it a email address
+    expect(user.toJSON()).toEqual({ email: 'test@test.com', _id: expect.any(Types.ObjectId) }); //expect user to equal to have email address and id
+  });
+  it('has a required email', () => {
+    const user = new User({});
+    const errors = user.validateSync().errors; //grabs errors
+    // console.log('ERRORS', errors);
+    expect(errors.email.message).toEqual('Path `email` is required.');
+  });
+  it('stores a _tempPassword', () => {
+    const user = new User({ email: 'wdifng@gmail.com', password: 'w345' }); //User is setting their password and usernamescreating a new user and passing in their email and password
+    expect(user._tempPassword).toEqual('w345');
+  });
+
+  it('has a passwordHash', () => {
+    // const user = new User({
+    //   email: 'test@test.com',
+    //   password:'wer'
+    // });
+    // user.save();
+
+    return User.create({
+      email: 'test@test.com',
+      password: 'p455w0rd',
+    })
+    .then(user => {
+      expect(user.passwordHash).toEqual(expect.any(String));
+      expect(user.password).toBeUndefined();
+    });
+  });
+  it('can compare good passwords', () => {
+    //create a new user
+    return User.create({
+      email: 'test@test.com',
+      password: 'p455w0rd'
+    })
+    //compare passwords
+    .then(user => {
+      return user.compare('p455w0rd'); //way of telling user passed in the correct password
+    })
+    .then(result => {
+      expect(result).toBeTruthy();
+    });
+  });
+
+  it('can compare bad passwords', () => {
+    return User.create({
+      email: 'test@test.com',
+      password: 'p455w0rd'
+    })
+    .then(user => {
+      return user.compare('badPassword'); //user inputs password and it does not match records
+    })
+    .then(result => {
+      expect(result).toBeFalsy();
+    });
+  });
+
+  it('can find a user token', () => { //give it a token and gives us a user back
+    //user has to exist before we can find a user Step 1- create a user
+    return User.create({
+      email: 'test@test.com',
+      password: 'p455w0rd'
+    })
+      .then(user => {
+        const token = tokenize(user);
+        return User.findByToken(token)
+      })
+      
+    //  .then(user => { //create a token out of that user
+    //    //create a token by the user with tokenize
+    //     const token = tokenize(user);
+    //     return User.findByToken(token);
+    //    //-> then findByToken(token)
+    //  })
+      .then(userFromToken => { //expect to find a user out of that tokenresult is everything that the created user has
+       expect(userFromToken).toEqual({
+         email: 'test@test.com',
+        //  passwordHash: expect.any(String),
+         _id: expect.any(String),
+        //  __v: 0
+       });
+     });
+  });
+  it('can create an authToken', () => {
+    return User.create({ //1st create user
+      email: 'weigh@yahoo.com',
+      password: 'pse95'
+    })
+    .then(user => user.authToken())  //after we have a user we want to
+    .then(token => {
+      expect(token).toEqual(expect.any(String));
+    });
+  }); 
+});
+
---
+++ test/utils/hash.test.js
@@ -0,0 +1,98 @@
+const bcrypt = require('bcryptjs');
+const { hash, compare } = require('../../lib/utils/hash');
+
+describe('hassing functions', () => {
+  it('hashes a password', () => {
+    return bcrypt.hash('password', 10) //run the packet imported and added salt runs function 10 times to generate random number
+    .then(hashedPassword => { //we are expecting a response of hashedPassword
+      expect(hashedPassword).toBeDefined(); //.toBeEqual('')empty string to be able to read the password
+    })
+  });
+
+  it('creates hashed passwords that are different', () => {
+    return bcrypt.hash('password', 10) 
+    .then(hashedPassword1 => { //can use a promise.all to make this nicer
+      return bcrypt.hash('password', 10)
+      .then(hashedPassword2 => {
+        expect(hashedPassword1).not.toEqual(hashedPassword2);
+    });
+    });
+  });
+  // it('creates the same hash given the same salt', () => {
+  //   return bcrypt.hash('password', '$2b$04$1236567850098765432123')
+  //   .then(hashedPasswordOne => {
+  //     return bcrypt.hash('password', salt)
+  //       .then(hashedPasswordTwo => {
+  //         expect(hashedPasswordOne).toEqual(hashedPasswordTwo);
+  //       });
+  //   });
+  // });
+  it('creates the same hash given the same salt', () => {
+    const password = 'passowrd';
+    const versionInfo = '$2b$10$'; //version and rounds
+    const salt = 'ASDFGTRHYJUIKOLPERTYUI';
+    const bcryptSalt = `${versionInfo}${salt}`; 
+    return bcrypt.hash('password', bcryptSalt) 
+      .then(hashedPassword => {
+        return Promise.all([ //pass this hasedPassword2 + has2
+          Promise.resolve(hashedPassword), //creates the fake promise that resolved with the hashedpassword
+          bcrypt.hash(password, bcryptSalt) 
+        ]);
+      })
+      .then(([hash1, hash2]) => {
+        expect(hash1).toEqual(hash1) //same has because of the same salg
+      });
+  });
+  it('can compare hashes basedon on the same password', () => {
+    const password = 'password'; 
+
+     return bcrypt.hash('password', 10) //return has from 
+       .then(hashedPassword => {
+          return bcrypt.compare(password, hashedPassword); 
+       })
+       .then(result => { //pass result 
+         expect(result).toBeTruthy();
+       });
+    //   .then(result => {
+    //     expect(result).toBeTruthy()
+    //   })
+  });
+
+  it('can compare hashes based on different password', () => {
+    return bcrypt.hash('password', 10) //returning the has password
+      .then(hashedPassword => {
+        return bcrypt.compare('badPassword', hashedPassword); 
+      })
+      .then(result => {
+        expect(result).toBeFalsy();
+      });
+  });
+
+  it('can hash a password', () => {
+    return hash('password') //passes it rhough function hash
+      .then(hashedPassword => {
+        expect(hashedPassword).toBeDefined();
+        expect(hashedPassword).not.toEqual('password');
+      });
+  });
+
+  it('compares password', () => {
+    return hash('password') //returning hash password hashed password here 
+      .then(hashedPassword => { //giving it some variable
+        return compare ('password', hashedPassword); 
+      })
+      .then(result => { 
+        expect(result).toBeTruthy();
+      });
+  });
+
+  it('can compare a bad password and string', () => {
+    return hash('password') //return the has 
+      .then(hashedPassword => { //take hashpassword
+        return compare('badpassword', hashedPassword);
+      })
+      .then(result => { //then then result should befalse
+        expect(result).toBeFalsy();
+      })
+  })
+});
