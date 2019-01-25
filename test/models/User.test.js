fcc1412b589f8bcd9a008cd9f4503eb28b6b03a3
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-24 16:57:42 -0800

    deletes version and password hash

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -54,9 +54,7 @@ describe('user model tests', () => {
       .then(userFromToken => {
         expect(userFromToken).toEqual({
           email: 'test@test.com',
-          passwordHash: expect.any(String),
-          _id: expect.any(String),
-          __v: 0
+          _id: expect.any(String)
         });
       });
   });
