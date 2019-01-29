ba6c1f1f5b5b153de57a6a279f7e25a4c91b2ea3
Kristin Hortsch <kristinhortsch@gmail.com>
2019-01-28 17:36:43 -0800

    passing ensureAuth test

---
+++ test/ensureAuth.test.js
@@ -0,0 +1,15 @@
+const { bearerToken, ensureAuth } = require('../lib/middleware/ensureAuth');
+describe('ensureAuth', () => {
+  it('can get a bearer token', () => {
+    const req = {
+      get: () => 'Bearer abcd1234'
+    };
+
+    const next = jest.fn();
+
+    bearerToken(req, {}, next);
+
+    expect(req.token).toEqual('abcd1234');
+    expect(next).toHaveBeenCalled();
+  });
+});
