50fd06301da9e77b1fc7980a7ac262331ff7a9af
pickuppatterns <pickup.patterns@gmail.com>
2019-01-29 14:19:41 -0800

    updates ansureAuth test

--- test /middleware/ensureAuth.test.js
+++ test /middleware/ensureAuth.test.js
@@ -1,40 +1,38 @@
+require('dotenv').config();
 const { bearerToken, ensureAuth } = require('../../lib/middleware/ensureAuth');
-const { tokenize } = require('../../lib/utils/token'); 
-
+const { tokenize } = require('../../lib/utils/token');
 
 describe('ensureAuth', () => {
-  it('can get a bearer token',  () => {
-    
+  it('can get a bearer token', () => {
+
     const req = {
       get: () => 'Bearer abcd1234'
     };
-    const next = jest.fn();
 
+    const next = jest.fn();
 
     bearerToken(req, {}, next);
 
     expect(req.token).toEqual('abcd1234');
     expect(next).toHaveBeenCalled();
-    
   });
-  it('can ensureAuth has a valid token', () => {
+
+  it('can ensureAuth', () => {
+    // use tokenize to create a token
     const token = tokenize({ email: 'test@test.com' });
 
+    // create req with the token
     const req = {
       token
     };
+
     const next = jest.fn();
 
-
     ensureAuth(req, {}, next)
-      //this makes ensure Auth async like the rest of the stuff we have
       .then(() => {
-
+        // expect req.user to equal the token payload
         expect(req.user).toEqual({ email: 'test@test.com' });
         expect(next).toHaveBeenCalled(1);
       });
-
   });
-
 });
-
