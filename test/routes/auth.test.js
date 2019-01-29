1f6ec47009a451656f74ea7292dab89297d8a45f
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-28 17:23:52 -0800

    Signin user route and test, with bad email test too

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 require('../../lib/utils/connect')();
-// const User = require('../../lib/models/User');
+const User = require('../../lib/models/User');
 const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../../lib/app');
@@ -8,6 +8,12 @@ const app = require('../../lib/app');
 //   tokenize,
 //   untokenize
 // } = require('../../lib/utils/token');
+
+// const createUser = (email, password) => {
+//   return request(app)
+//     .post('/auth/signup')
+
+// };
 
 
 describe('auth route', () => {
@@ -20,7 +26,8 @@ describe('auth route', () => {
     mongoose.connection.close();
     done();
   });
-  it('can create an authToken', done => {
+
+  it('can sign up a new user and include token', done => {
     const user = {
       email: 'test@test.com',
       password: 'password'
@@ -39,4 +46,48 @@ describe('auth route', () => {
         done();
       });
   });
+
+  it('can sign in a user', () => {
+    return User.create({
+      email: 'b@b.com',
+      password: 'booboo'
+    })
+      .then(createdUser => {
+        return request(app)
+          .post('/auth/signin')
+          .send({
+            email: 'b@b.com',
+            password: 'booboo'
+          })
+          .then(res => {
+            expect(res.body).toEqual({
+              user: {
+                _id: createdUser._id.toString(),
+                email: 'b@b.com'
+              },
+              token: expect.any(String)
+            });
+          });
+      })
+      .catch(console.log);
+  });
+
+  it('can respond with message if bad email or password', done => {
+    return User.create({
+      email: 'b@b.com',
+      password: 'booboo'
+    })
+      .then(() => {
+        return request(app)
+          .post('/auth/signup')
+          .send({ email: 'bad', password: 'monkey' })
+          .then(res => {
+            expect(res.body).toEqual({
+              error: 'User validation failed: email: \'bad\' is not a valid email!'
+            });
+            done();
+          });
+      });
+  });
+
 });
