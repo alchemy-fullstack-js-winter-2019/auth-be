0282985203d133816911c37bcd3d5c38a4f2adcd
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-28 16:45:24 -0800

    updated app and made sign up on apptest

--- test/app.test.js
+++ test/app.test.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
-const connect = require();
+const connect = require('../lib/utils/connect');
 
 const request = require('supertest');
 const mongoose = require('mongoose');
 
+const app = require('../lib/app');
+
 describe('app', () => {
+  beforeAll(() => {
+    connect();
+  });
+
   beforeEach(done => {
     mongoose.connection.dropDatabase(done);
   });
@@ -14,6 +20,17 @@ describe('app', () => {
   });
 
   it('can /signup a user', () => {
-    
+    return request(app)
+      .post('/auth/signup')
+      .send({ email: 'test@test.com', password: 'password' })
+      .then(res => {
+        expect(res.body).toEqual({
+          user: {
+            _id: expect.any(String),
+            email: 'test@test.com'
+          },
+          token: expect.any(String)
+        });
+      });
   });
 });
