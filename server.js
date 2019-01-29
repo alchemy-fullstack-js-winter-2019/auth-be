742484f28cfbb7a87b796087c180ddef2680ecb2
Marcy Silverman <marcysilve225@gmail.com>
2019-01-28 18:08:08 -0800

    attempted the ensure auth

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ require('./lib/utils/connect')();
 const app = require('./lib/app');
 
 app.listen(7890, () => {
+    /* eslint-disable-next-line */
     console.log('running');
 });
---
+++ test/utils/ensureAuth.test.js
@@ -0,0 +1,23 @@
+const { bearerToken, ensureAuth } = require('../../lib/middleware/ensureAuth');
+
+describe('ensureAuth', () => {
+    it('can get a bearer token', () => {
+        const req = {
+            get: () => 'Bearer abcd1234'
+
+        };
+        const next = jest.fn();
+        bearerToken(req, {}, next);
+        expect(req.token).toEqual('abcd1234');
+        expect(next).toHaveBeenCalled();
+    });
+    it('can ensure an auth', () => {
+        const req = {
+            get: () => 'abcd1234'
+        };
+        const next = jest.fn();
+        ensureAuth(req, {}, next);
+        expect(req.token).toEqual('abcd1234');
+        expect(next).toHaveBeenCalled();
+    });
+});
