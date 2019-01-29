fbffea51ee35bf280cec1cb229850dbeacfb3919
Carmen Perezchica <crperezchica@gmail.com>
2019-01-29 12:47:13 -0800

    passing signin and ensure auth tests

--- test/auth.test.js
+++ test/auth.test.js
@@ -4,9 +4,15 @@ const request = require('supertest');
 const app = require('../lib/app');
 const mongoose = require('mongoose');
 // const auth = require('../lib/routes/auth');
+const User = require('../lib/models/User');
 
 
 describe('', () => {
+  // const createUser = (email, password) => {
+  //   return request (app)
+  //     .post('/auth/')
+  // }
+
   beforeAll(() => {
     connect();
   });
@@ -37,12 +43,56 @@ describe('', () => {
       });
   });
 
-  // it('', () => {
+  it('signin a user', () => {
+    return User.create({ email: 'bob@yahoo.com', password: 'qwert' }) //this is a promise. created a user
+      .then(() => {
+        return request(app)
+          .post('/auth/signin')
+          .send({ 
+            email: 'bob@yahoo.com',
+            password: 'qwert'
+          });
+      })
+      .then (res => {
+        expect(res.body).toEqual({
+          user: {
+            _id: expect.any(String),
+            email: 'bob@yahoo.com'
+          },
+          token: expect.any(String)
+        });
+      });
+  });
+  it('has a /verify route', () => { 
+    //create a user
+    return User.create({ email: 'bob@yahoo.com', password: 'wert' })
+    //log user in to get a token
+      .then(() => {
+        //login user to get a token
+        return request(app)
+          .post('/auth/signin')
+          .send({ email: 'bob@yahoo.com', password: 'wert' })
+          .then(res => res.body.token);
+      })
+      .then(token => {
+        //request /verify and present token
+        //use .set(Authorization',`Bear ${token})
+        return request(app)
+          .get('/auth/verify')
+          .set('Authorization', `Bearer ${token}`);
+      })
+      .then(res => {
+        //expect res.body to be a user
+        expect(res.body).toEqual({
+          email: 'bob@yahoo.com',
+          _id: expect.any(String)
+        });
+      });
 
-  // });
+  });
+
   afterAll(done => {
     mongoose.connection.close(done);
   });
 
 });
-
--- test/ensureAuth.test.js
+++ test/ensureAuth.test.js
@@ -1,17 +1,37 @@
+require('dotenv').config();
 const { bearerToken, ensureAuth } = require('../lib/middleware/ensureAuth');
+const { tokenize } = require('../lib/utils/token');
 
 describe('ensureAuth', () => {
   it('can get a bearer token', () => {
 
     const req = {
-      get: () => 'Bearer abcd1234'
+      get: () => 'Bearer abcd1234' //get is the key..property that is a function equvalent to get() { return 'Bearer abcd1234'}
     };
 
-    const next = jest.fn();
+    const next = jest.fn(); //using this jest function to call the next function
 
-    bearerToken(req, {}, next); //passing
+    bearerToken(req, {}, next); //response just sending an empty object
 
     expect(req.token).toEqual('abcd1234');
     expect(next).toHaveBeenCalled();
+  });
+
+  it('can ensureAuth', () => { //this should get us a user back
+    //use tokenize to create a token
+    const token = tokenize ({ email: 'pony@yahoo.com' });
+    //create req with the token
+    const req = {
+      token
+    };
+    //expect req.user to equal the token payload
+    const next = jest.fn(); //creates a fake function that lets you know how many times it's been called
+    ensureAuth(req, {}, next)
+      .then(() => {
+        expect(req.user).toEqual({ email: 'pony@yahoo.com' });
+        expect(next).toHaveBeenCalled();
+
+
+      });
   });
 });
