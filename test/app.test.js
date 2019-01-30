19e003fd4d4e662f24917a7392813f3d2e3af0d5
Sophie Graham <sophiergraham1@gmail.com>
2019-01-30 10:06:09 -0800

    not done yet

---
+++ test/app.test.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const connect = require('../lib/utils/connect');
+const request = require('supertest');
+const mongoose = require('mongoose');
+const app = require('../lib/app');
+
+describe('app', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(done => {
+    mongoose.connection.dropDatabase(done);
+  });
+
+  afterAll(done => {
+    mongoose.connection.close(done);
+  });
+
+  it('can /signup a user', () => {
+    return request(app)
+      .post('/auth/signup')
+      .send({ email: 'test@test.com', password: 'password' })
+      .then(res => {
+        expect(res.body).toEqual({
+          user: {
+            _id: expect.any(String),
+            email: 'test@test.com'
+          },
+          token: expect.any(String)
+        });
+      });
+  });
+
+});
