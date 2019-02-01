69d6ff4eb6364da0132affc9a46750ee55b35caf
Sophie Graham <sophiergraham1@gmail.com>
2019-02-01 14:48:36 -0800

    passing sign in a user

--- test/app.test.js
+++ test/app.test.js
@@ -3,6 +3,7 @@ const connect = require('../lib/utils/connect');
 const request = require('supertest');
 const mongoose = require('mongoose');
 const app = require('../lib/app');
+const User = require('../lib/models/User');
 
 describe('app', () => {
   beforeAll(() => {
@@ -21,6 +22,27 @@ describe('app', () => {
     return request(app)
       .post('/auth/signup')
       .send({ email: 'test@test.com', password: 'password' })
+      .then(res => {
+        expect(res.body).toEqual({
+          user: {
+            _id: expect.any(String),
+            email: 'test@test.com'
+          },
+          token: expect.any(String)
+        });
+      });
+  });
+
+  it('can /signin a user', () => {
+    return User.create({
+      email: 'test@test.com',
+      password: 'password'
+    })
+      .then(() => {
+        return request(app)
+          .post('/auth/signin')
+          .send({ email: 'test@test.com', password: 'password' });
+      })
       .then(res => {
         expect(res.body).toEqual({
           user: {
